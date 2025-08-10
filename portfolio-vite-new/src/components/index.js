411b235f5e45fecf7d5b489e69675eddbee7e7eb
Ramkarthick1525 <ramkarthickd1525@gmail.com>
2025-08-10 16:52:39 +0530

    ok

--- portfolio-vite-new/src/components/index.js
+++
@@ -1,9 +0,0 @@
-import About from './About';
-import Contact from './Contact';
-import Home from './Home';
-import NavBar from './NavBar';
-import Portfolio from './Portfolio';
-import Skills from './Skills';
-import SocialLinks from './SocialLinks';
-
-export { About, Contact, Home, NavBar, Portfolio, Skills, SocialLinks };
